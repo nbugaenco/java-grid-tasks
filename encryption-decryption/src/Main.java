a0623580495e2df512083b5c3a6fa5aa3ce1633e
nbugaenco <nbugaenco@griddynamics.com>
2023-03-03 14:28:35 +0200

    Now it's working with CLI args

--- encryption-decryption/src/Main.java
+++ encryption-decryption/src/Main.java
@@ -1,16 +1,41 @@
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Map<String, String> params = new HashMap<>() {{
+            put("-mode", "enc");
+            put("-key", "0");
+            put("-data", "");
+        }};
 
-        String action = scanner.nextLine();
+        for (int i = 0; i < args.length - 1; ++i) {
+            switch (args[i]) {
+                case "-mode" -> {
+                    if (args[i + 1].equals("enc") || args[i + 1].equals("dec")) {
+                        params.replace(args[i], args[i + 1]);
+                    } else {
+                        System.out.println("Wrong mode!\nModes available: enc, dec\n");
+                        System.exit(0);
+                    }
+                }
+                case "-key" -> {
+                    try {
+                        int key = Integer.parseInt(args[i + 1]);
+                        params.replace(args[i], Integer.toString(key));
+                    } catch (NumberFormatException e) {
+                        System.out.println("Wrong key!\nIt must be an integer number.\nYour key is set to 0\n");
+                    }
+                }
+                case "-data" -> params.replace(args[i], args[i + 1]);
+            }
+        }
 
-        switch (action) {
-            case "enc" -> System.out.println(encryptString(scanner.nextLine(), scanner.nextInt()));
-            case "dec" -> System.out.println(decryptString(scanner.nextLine(), scanner.nextInt()));
-            default -> System.out.println("Wrong action!");
+        if (params.get("-mode").equals("enc")) {
+            System.out.println(encryptString(params.get("-data"), Integer.parseInt(params.get("-key"))));
+        } else {
+            System.out.println(decryptString(params.get("-data"), Integer.parseInt(params.get("-key"))));
         }
     }
 
