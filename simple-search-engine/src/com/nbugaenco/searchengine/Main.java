c1a4ca5aa4c8bef3e1c52b8de63a909f36ffe250
nbugaenco <nbugaenco@griddynamics.com>
2023-03-09 12:29:42 +0200

    Added menu

--- simple-search-engine/src/com/nbugaenco/searchengine/Main.java
+++ simple-search-engine/src/com/nbugaenco/searchengine/Main.java
@@ -19,24 +19,54 @@ public class Main {
             lines.add(scanner.nextLine());
         }
 
-        System.out.print("\nEnter the number of search queries: ");
-        int searchCount = scanner.nextInt();
-        scanner.nextLine();
+        byte choice;
+        do {
+            System.out.println(getMenu());
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextByte();
+            scanner.nextLine();
 
-        for (int i = 0; i < searchCount; ++i) {
-            System.out.print("\nEnter data to search people: ");
-            String searchQuery = scanner.nextLine();
+            processChoice(lines, choice);
+        } while (choice != 0);
+    }
 
-            List<String> tmp = lines.stream()
-                    .filter(l -> l.toLowerCase().contains(searchQuery.toLowerCase()))
-                    .toList();
+    public static void processChoice(List<String> lines, byte choice) {
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println(
-                    (tmp.size() > 0) ? "\nFound people:" : "\nNo matching people found."
-            );
-
-            tmp.forEach(System.out::println);
+        switch (choice) {
+            case 1 -> {
+                System.out.println("\nEnter a name or email to search all suitable people.");
+                String searchQuery = scanner.nextLine();
+                System.out.println(findAPerson(lines, searchQuery));
+            }
+            case 2 -> {
+                System.out.println("\n=== List of people ===");
+                lines.forEach(System.out::println);
+            }
+            case 0 -> System.out.println("\nBye!");
+            default -> System.out.println("\nIncorrect option! Try again.");
         }
+    }
 
+    public static String findAPerson(List<String> lines, String searchQuery) {
+        List<String> tmp = lines.stream()
+                .filter(l -> l.toLowerCase().contains(searchQuery.toLowerCase()))
+                .toList();
+
+        StringBuilder result = new StringBuilder((tmp.size() > 0) ? "\n=== Found people ===\n" : "\nNo matching people found.");
+
+        tmp.forEach(s -> result.append(s).append("\n"));
+
+        return result.toString();
+    }
+
+    public static String getMenu() {
+        return """
+                
+                === Menu ===
+                1. Find a person
+                2. Print all people
+                0. Exit
+                """;
     }
 }
